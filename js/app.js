8078d519d64d550ec477ca37b4c7f8e7495e4d07
Ioannis Chatzinikolaou <i.chatzinikolaou@afse.eu>
2021-12-03 17:32:43 +0200

    hghhg

---
+++ js/app.js
@@ -0,0 +1,11 @@
+console.error('Error testing');
+
+function turnOn(){
+    var lamp = document.getElementById('lamp');
+    lamp.src = './images/2.jpg'
+}
+
+function turnOff(){
+    var lamp = document.getElementById('lamp');
+    lamp.src = './images/1.jpg'
+}
